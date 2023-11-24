c9b88c1722ad806de43b53f129113f60e12ceb5b
Ozael <ozaelvf@gmail.com>
2023-11-23 21:29:03 -0300

    properties of the cards added

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover = '';
 
-  constructor() { }
+  @Input()
+  title = '';
+
+  @Input()
+  description = '';
+
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.description.length > 200) {
+      this.description = `${this.description.substring(0, 200)}...`;
+    }
   }
-
 }
